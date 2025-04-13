604b40149bcecf2383d5f77b7649324cec33a8ea
Sahith Aitha <aithasahith0214@gmail.com>
2025-04-13 18:22:30 -0400

    AWS-EKS: add Terraform module to provision AWS EKS cluster  - Imported official terraform-aws-modules/eks/aws module  - Created managed node group using t2.micro for free-tier eligibility  - Configured inputs for cluster name, VPC ID, and subnet IDs  - Output EKS cluster name and endpoint

---
+++ terraform/eks/main.tf
@@ -0,0 +1,27 @@
+# terraform/eks/main.tf
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 19.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = var.subnet_ids
+  vpc_id          = var.vpc_id
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t2.micro"]
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+    }
+  }
+
+  tags = {
+    Project = var.cluster_name
+  }
+}
+
---
+++ terraform/eks/variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
